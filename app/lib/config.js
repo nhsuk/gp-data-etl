c7bf1eaca6bf58a83ddd5964672f51273057c6a4
mark <markharrop@yahoo.com>
2017-09-11 15:18:12 +0100

    :hourglass: when disabled set run time to 100 years in the future

--- app/lib/config.js
+++ app/lib/config.js
@@ -1,9 +1,17 @@
+function getRule() {
+  if (process.env.DISABLE_SCHEDULER === 'true') {
+    return new Date(2117, 11, 31, 23, 59);
+  }
+  return process.env.ETL_SCHEDULE || '0 23 * * *';
+}
+
 const config = {
   hitsPerHour: 5000,
   saveEvery: 100,
   outputDir: './output',
   outputName: 'gp-data',
   containerName: process.env.CONTAINER_NAME || 'etl-output',
+  getRule,
 };
 
 module.exports = config;
--- schedule.js
+++ schedule.js
@@ -1,15 +1,9 @@
 const schedule = require('node-schedule');
-
+const config = require('./app/lib/config');
 const etl = require('./app/lib/etl');
 const log = require('./app/lib/utils/logger');
 
-const rule = process.env.ETL_SCHEDULE || '0 23 * * *';
-const disabled = process.env.DISABLE_SCHEDULER === 'true';
-if (disabled) {
-  log.info('Scheduler disabled');
-} else {
-  log.info(`Scheduling job with rule '${rule}'`);
-  schedule.scheduleJob(rule, () => {
-    etl.start();
-  });
-}
+log.info(`Scheduling job with rule '${config.getRule()}'`);
+schedule.scheduleJob(config.getRule(), () => {
+  etl.start();
+});
